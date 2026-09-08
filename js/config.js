c078317ae4bec2f7cfc67fa8728ce6e8f5f2803c
larissapereiras-del <larissa.pereiras@mercadolivre.com>
2026-09-07 21:32:30 -0300

    Update Supabase URL and ANON key in config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 const SUPABASE_URL =
-  "sb_publishable_02RDlrIN-ppJOfrZAy-1DQ_HLizQvbZ";
+ "https://ifbecuswojeufycovodo.supabase.co";
 
 const SUPABASE_ANON_KEY =
-  "COLE_AQUI_SUA_ANON_KEY";
+  "sb_publishable_02RDlrIN-ppJOfrZAy-1DQ_HLizQvbZ";
 
 
 const db =
